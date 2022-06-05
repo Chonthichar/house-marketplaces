e0021230c2f0e01ea6cba1d9ad7558d0db12f2b5
Chonthichar <chonthichar.soythong@gmail.com>
2022-06-05 15:25:57 +0700

    styling

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -15,22 +15,25 @@ import Contact from "../components/Contact";
 
 const slideImages = [
     {
-        url: 'https://scontent.fsvg1-1.fna.fbcdn.net/v/t39.30808-6/240227290_610021287051111_6758305064036924612_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_ohc=d7A4h4p62_gAX9iqVvA&_nc_ht=scontent.fsvg1-1.fna&oh=00_AT-HcOlIGML3PVecylbKQgvMgkyfw7yo-0gxzQhQs1Z8wg&oe=629F67EE',
+        url: 'https://scontent.fosl4-2.fna.fbcdn.net/v/t39.30808-6/285861541_5466311896766323_2503748729812327999_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_ohc=oBl3lT8gvrkAX9eALYi&_nc_ht=scontent.fosl4-2.fna&oh=00_AT8ymoPTOb-Sey-u5tIFO4Op2q426mo7kg9sTDxd0hT6mA&oe=62A1AEC6',
         caption: <p className=''>
             HuaHin DreamHome <br/> PoolVilla
         </p>
 
     },
     {
-        url: 'https://scontent.fsvg1-1.fna.fbcdn.net/v/t39.30808-6/241365277_645127333540506_6846923631458127254_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=G6DhyYNpnHMAX98tjsX&_nc_ht=scontent.fsvg1-1.fna&oh=00_AT80_G5XtLFRjTpg8im2qJkxe7-svfaHnAj5pmVtH21RDQ&oe=629DB9D9',
+        url: 'https://scontent.fosl4-2.fna.fbcdn.net/v/t39.30808-6/286199545_5466311976766315_6313532079394731312_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_ohc=IsxZExvfPxAAX_12bmC&_nc_ht=scontent.fosl4-2.fna&oh=00_AT-hKRVvODRDU2p01BbrP1CS_UscbCDHhCGnWRur1mhuNg&oe=62A09F52',
         // caption: <p className="">Eat healthy, Stay Healthy.</p>,
 
     },
     {
-        url: 'https://scontent.fsvg1-1.fna.fbcdn.net/v/t39.30808-6/241238765_619616146091625_2337374229408274435_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_ohc=tg1Tj8I5hl0AX9fVlJI&_nc_ht=scontent.fsvg1-1.fna&oh=00_AT_Pyr6uHkx88PKJum3O0sSsZIrBQi9TgsKmxumC-C8IGw&oe=629F032C',
+        url: 'https://scontent.fosl4-2.fna.fbcdn.net/v/t39.30808-6/286302279_5466312050099641_626295631910343258_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_ohc=6V29-YUQB10AX_NmhF9&_nc_ht=scontent.fosl4-2.fna&oh=00_AT-ZOTnXbmgDjO_D5xvIBXJPbX6HP77qtoxmtRpa917aSg&oe=62A0D311',
         caption: <h2 className="be-prepare">"Sell - Rent
             <br/> Houses - Condos - Land</h2>
     },
+    // {
+    //     url: 'https://scontent.fosl4-2.fna.fbcdn.net/v/t39.30808-6/286302279_5466312050099641_626295631910343258_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_ohc=6V29-YUQB10AX_NmhF9&_nc_ht=scontent.fosl4-2.fna&oh=00_AT-ZOTnXbmgDjO_D5xvIBXJPbX6HP77qtoxmtRpa917aSg&oe=62A0D311',
+    // },
 ];
 
 function Features() {
