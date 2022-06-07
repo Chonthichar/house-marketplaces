35640d06ea2fdb90ab539112f04cefd1ad95f541
Chonthichar <chonthichar.soythong@gmail.com>
2022-06-08 04:13:19 +0700

    styling

--- src/components/QuickViewA.jsx
+++ src/components/QuickViewA.jsx
@@ -165,13 +165,13 @@ export default function Example() {
 
                 {/* Product info */}
                 <div
-                    className="max-w-2xl mx-auto pt-10 pb-16 px-4 sm:px-6 lg:max-w-7xl lg:pt-16 lg:pb-24 lg:px-8 lg:grid lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8">
+                    className="max-w-2xl mx-auto pt-10 pb-3 px-4 sm:px-6 lg:max-w-7xl lg:pt-16 lg:pb-24 lg:px-8 lg:grid lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8">
                     <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
                         <h1 className="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">{product.name}</h1>
                     </div>
 
                     {/* Options */}
-                    <div className="mt-4 lg:mt-0 lg:row-span-3">
+                    <div className="mt-0 lg:mt-0 lg:row-span-3" >
                         <h2 className="sr-only">Product information</h2>
                         <p className="text-3xl text-gray-900">{product.price}</p>
 
@@ -314,7 +314,7 @@ export default function Example() {
                     </div>
 
                     <div
-                        className="py-10 lg:pt-6 lg:pb-16 lg:col-start-1 lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8 pb-0 mb-0">
+                        className="py-10 lg:pt-6 lg:pb-16 lg:col-start-1 lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8 pb-0 mb-0   ">
                         {/* Description and details */}
                         <div>
                             <h3 className="sr-only">Description</h3>
