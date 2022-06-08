4f58f1281093a241ba8d8b27a1c50934d3a70a05
Chonthichar <chonthichar.soythong@gmail.com>
2022-06-08 13:50:17 +0700

    styling

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -21,17 +21,18 @@ import QuickViewD from "../components/QuickViewD"
 import QuickViewE from "../components/QuickViewE"
 
 const slideImages = [
-    {
-        url: 'https://scontent.fsvg1-1.fna.fbcdn.net/v/t39.30808-6/286313110_5466311866766326_3813590309502438832_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_ohc=ML1y6Ns1x5IAX80K67G&tn=IcFkUDcwWIuuHVHs&_nc_ht=scontent.fsvg1-1.fna&oh=00_AT91MCAZOh-FnT-nhYbS_X4T7hiXDU1PCNTh1SwjNrVohw&oe=62A320C6',
-        caption: <h1 className=''>HuaHin DreamHome PoolVilla
+      {
+        url: 'https://scontent.fsvg1-1.fna.fbcdn.net/v/t39.30808-6/286306296_5466311770099669_4302134959182240249_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_ohc=p64OwydWRMUAX8Arcmc&_nc_ht=scontent.fsvg1-1.fna&oh=00_AT_DdL6rg0e8aaVzKakc-Kd-A7P6JpMGVHa0sADhamCHIg&oe=62A33ABB',
+        caption:  <h1 className=''>HuaHin DreamHome PoolVilla
             <br/> +66 (0) 63-869-7817
             </h1>
-
     },
     {
-        url: 'https://scontent.fsvg1-1.fna.fbcdn.net/v/t39.30808-6/286306296_5466311770099669_4302134959182240249_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_ohc=p64OwydWRMUAX8Arcmc&_nc_ht=scontent.fsvg1-1.fna&oh=00_AT_DdL6rg0e8aaVzKakc-Kd-A7P6JpMGVHa0sADhamCHIg&oe=62A33ABB',
-        caption: <p className="">Sell & Rent
+        url: 'https://scontent.fsvg1-1.fna.fbcdn.net/v/t39.30808-6/286313110_5466311866766326_3813590309502438832_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_ohc=ML1y6Ns1x5IAX80K67G&tn=IcFkUDcwWIuuHVHs&_nc_ht=scontent.fsvg1-1.fna&oh=00_AT91MCAZOh-FnT-nhYbS_X4T7hiXDU1PCNTh1SwjNrVohw&oe=62A320C6',
+        caption:
+        <p className="">Sell & Rent
         <br/>Land - House - Condo</p>
+
     },
     {
         url: 'https://scontent.fsvg1-1.fna.fbcdn.net/v/t39.30808-6/286295249_5466311760099670_8435803177546229097_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_ohc=AkuI9s01-2gAX_uceC8&_nc_ht=scontent.fsvg1-1.fna&oh=00_AT9nfjys1OkpKzneaPUpsbnNkTKWbTSt34W_wp1FDhgrJQ&oe=62A4B069',
